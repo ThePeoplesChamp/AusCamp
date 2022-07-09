c9aa88b91285116a2bee65a9ecef525a709dd180
Peoples Champion <95397105+ThePeoplesChamp@users.noreply.github.com>
2022-07-09 12:39:11 +0930

    final Deploy

--- routes/reviews.js
+++ routes/reviews.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router({ mergeParams: true });
 const { validateReview, isLoggedIn, isReviewAuthor } = require('../middleware');
 const reviews = require('../controllers/reviews');
-const catchAsync = require('../utils/catchAsync');
+const catchAsync = require('../utils/CatchAsync');
 
 router.post('/', isLoggedIn, validateReview, catchAsync(reviews.createReview))
 
